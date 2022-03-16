0a44ad896a04bf91a4b764de49cdec23ab312696
jSdCool <37940266+jSdCool@users.noreply.github.com>
2022-03-16 19:48:54 -0400

    list command and proper disconnect from server

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.io.BufferedReader;
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
@@ -32,9 +30,11 @@ import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.jsdcool.discompnet.CComandList;
 import net.jsdcool.discompnet.CDiscordMessageData;
 import net.jsdcool.discompnet.CMinecraftMessageData;
 import net.jsdcool.discompnet.CompanionData;
+import net.jsdcool.discompnet.CShutdownData;
 
 public class Main extends ListenerAdapter implements ActionListener, WindowListener{
 
@@ -44,13 +44,11 @@ public class Main extends ListenerAdapter implements ActionListener, WindowListe
 	static String token ="",channelid="",guildid="";
 	static JDA jda;
 	public static MessageChannel chatChannel;
-	//static PrintStream ps;
 	static JFrame frame;
 	static JPanel panel;
 	static JLabel status,passwordLabel,success;
 	static JButton shutdownButton;
 	static boolean connected=false;
-	//static BufferedReader br;
 	static String statusMessage="status: starting";
 	static ObjectOutputStream output;
 	static ObjectInputStream input;
@@ -148,40 +146,37 @@ public class Main extends ListenerAdapter implements ActionListener, WindowListe
         // server executes continuously 
         while (true) { 
         	
-  
-            CompanionData send=dataToSend;
-            dataToSend=new CompanionData();
-            output.writeObject(send);
-            output.flush();
-            output.reset();
-            CompanionData dataIn=(CompanionData)input.readObject();
-            for(int i=0;i<dataIn.data.size();i++) {
-            	if(dataIn.data.get(i) instanceof CMinecraftMessageData msg) {
-            		chatChannel.sendMessage(msg.message).queue();
-            	}
-            }
-           
-            /*
+        	try {
+        		if(s.isConnected()&&!s.isClosed()) {
+        		
+        	
+        			CompanionData send=dataToSend;
+        			dataToSend=new CompanionData();
+        			output.writeObject(send);
+        			output.flush();
+        			output.reset();
+        			CompanionData dataIn;
+        			try {
+        				dataIn=(CompanionData)input.readObject();
+        				for(int i=0;i<dataIn.data.size();i++) {
+        					if(dataIn.data.get(i) instanceof CMinecraftMessageData msg) {
+        						chatChannel.sendMessage(msg.message).queue();
+        					}
+        					if(dataIn.data.get(i) instanceof CShutdownData) {
+        						socketDisconnect();
+        					}
+        				}
+        			}catch(EOFException e) {
+        					socketDisconnect();
+        			}
             
-            		
-                
-            	}
-            	if(str.contains("<info>§")) {
-            		if(str.substring(7,str.length()).equals("<shutdown>")) {
-            			status.setText("status: disconnected");
-            			connected=false;
-            			s.close();
-            			ss.close();
-            			output.close();
-            			input.close();
-            			s=null;
-            			ss=null;
-            			createSocket();
-            		}
-            	}
-            }
-            */
-             
+           
+        		}else {
+        			socketDisconnect();
+        		}
+        	}catch(Exception e) {
+        		socketDisconnect();
+        	}
             
             
             
@@ -231,6 +226,12 @@ public class Main extends ListenerAdapter implements ActionListener, WindowListe
             return;
         }
         
+        if(content.equals("/list")) {
+        	dataToSend.data.add(new CComandList());
+        	System.out.println("sending list command");
+        	return;
+        }
+        
         String name;
         if(event.getMember().getNickname()==null){
             name = author.getName();
@@ -242,7 +243,6 @@ public class Main extends ListenerAdapter implements ActionListener, WindowListe
         for(int i=roles.size()-1;i>=0;i--) {
         	if(536870911!=roles.get(i).getColorRaw())
         	roleColor = roles.get(i).getColorRaw();
-        	System.out.println(roles.get(i).getName()+" "+ roles.get(i).getColorRaw());
         }
         
        dataToSend.data.add(new CDiscordMessageData(content,author.getName(),name,author.getId(),roleColor));
@@ -305,6 +305,22 @@ public class Main extends ListenerAdapter implements ActionListener, WindowListe
 		}catch(Throwable e) {}
 		System.out.println("shutting down");
 		System.exit(0);
+	}
+	
+	static void socketDisconnect() throws Throwable {
+		try {
+		status.setText("status: disconnected");
+		connected=false;
+		s.close();
+		ss.close();
+		output.close();
+		input.close();
+		s=null;
+		ss=null;
+		createSocket();
+		}catch(Exception e) {
+			
+		}
 	}
 
 }
